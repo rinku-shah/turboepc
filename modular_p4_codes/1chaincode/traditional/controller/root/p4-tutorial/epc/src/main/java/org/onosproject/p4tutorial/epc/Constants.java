3f85c8aa300a9327149f110010cf178b73af8505
coolvikas <mailvikasnow@gmail.com>
2019-04-03 15:56:33 +0530

    Changed encryption flags to true

--- modular_p4_codes/1chaincode/traditional/controller/root/p4-tutorial/epc/src/main/java/org/onosproject/p4tutorial/epc/Constants.java
+++ modular_p4_codes/1chaincode/traditional/controller/root/p4-tutorial/epc/src/main/java/org/onosproject/p4tutorial/epc/Constants.java
@@ -206,8 +206,8 @@ public class Constants {
 			final static int SGW_DPID_4 = 3;*/
 			
 			// boolean flags which control whether encryption and integrity checks needs to be performed or not.
-			static boolean DO_ENCRYPTION = false;
-			static boolean CHECK_INTEGRITY = false;
+			static boolean DO_ENCRYPTION = true;
+			static boolean CHECK_INTEGRITY = true;
 		//=====================================
 			// DPID or unique ID of default switch 
 			final static int DEFAULT_SWITCH_ID_1 = 1;
