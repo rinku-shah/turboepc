95b67509fd47f9a1799bdd2520268d4d90bbd7d0
rinku-shah <rinku@cse.iitb.ac.in>
2020-01-06 16:56:31 +0530

    updated the firmware load files

--- smartnic/sosr/offload/p4-code/basic_without_gtp_sgw/load_sgw.sh
+++ smartnic/sosr/offload/p4-code/basic_without_gtp_sgw/load_sgw.sh
@@ -1,2 +1,4 @@
-sudo ./rtecli design-load -f ~/oracle/smartnic_newmapping/p4-code/basic_without_gtp/basic_without_gtp_sgw/out/turboepc_sgw.nffw -p ~/oracle/smartnic_newmapping/p4-code/basic_without_gtp/basic_without_gtp_sgw/out/pif_design.json -c ~/oracle/smartnic_newmapping/p4-code/basic_without_gtp/basic_without_gtp_sgw/sgw.p4cfg
+sudo ./rtecli design-load -f ~/basic_without_gtp_sgw/out/turboepc_sgw.nffw -p ~/basic_without_gtp_sgw/out/pif_design.json -c ~/basic_without_gtp_sgw/sgw.p4cfg
+
+#sudo /opt/nfp_pif/bin/pif_rte -n 0 -p 20206 -I -z -s /opt/nfp_pif/scripts/pif_ctl_nfd.sh -d ~/basic_without_gtp_sgw/out/pif_design.json -f ~/basic_without_gtp_sgw/out/turboepc_sgw.nffw -c ~/basic_without_gtp_sgw/sgw.p4cfg
 
